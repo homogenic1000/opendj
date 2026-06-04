d979a032e560eb1627ceae3960f95dc1bb8ff367
Mathéo Delessert <pl06uj@eduvaud.ch>
2026-06-04 14:22:31 +0200

    truc muche

--- sticky.js
+++ sticky.js
@@ -1,16 +1,20 @@
-var app = document.getElementById('app');
+document.addEventListener('DOMContentLoaded', () => {
+    var app = document.getElementById('app');
 
-var typewriter = new Typewriter(app, {
-  loop: true,
-  delay: 75,
+    var typewriter = new Typewriter(app, {
+        loop: true,
+        delay: 75
+    });
+
+  typewriter
+    .pauseFor(1200)
+    .typeString('Pour une fête libre.')
+    .pauseFor(1600)
+    .deleteChars(7)
+    .typeString(' accessible.')
+    .pauseFor(1600)
+    .deleteChars(11)
+    .typeString(' meilleure.')
+    .pauseFor(1600)
+    .start();
 });
-
-typewriter
-  .pauseFor(2500)
-  .typeString('A simple yet powerful native javascript')
-  .pauseFor(300)
-  .deleteChars(10)
-  .typeString('<strong>JS</strong> plugin for a cool typewriter effect and ')
-  .typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
-  .pauseFor(1000)
-  .start();
